ab92533be670c4659bd0405aee906938e6b421ef
Victoria <berling2818@gmail.com>
2021-03-17 23:20:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import change from "./exchange.svg";
+import plus from "./plus-cross.svg";
+import minus from "./minus.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+    };
+  }
+  handleClick = () => {
+    this.setState({
+      index: ++this.state.index,
+    });
+  };
+  handleClickChange = () => {
+    this.setState({
+      index: 0,
+    });
+  };
+
+  handleClickMinus = () => {
+    this.setState({
+      index: this.state.index - 1,
+    });
+  };
+
+  render() {
+    let valid = this.state.index;
+
+    if (valid < 0) {
+      valid = 0;
+      <p className="App-text">{valid}</p>;
+    } else {
+    }
+    return (
+      <div className="App-counter">
+        <p className="App-text">{valid}</p>
+        <div className="App-button">
+          <button className="App-plus" onClick={this.handleClick}>
+            <img src={plus} alt={"plus"}></img>
+          </button>
+          <button className="App-change" onClick={this.handleClickChange}>
+            <img src={change} alt={"change"}></img>
+          </button>
+          <button className="App-minus" onClick={this.handleClickMinus}>
+            <img src={minus} alt={"minus"}></img>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
